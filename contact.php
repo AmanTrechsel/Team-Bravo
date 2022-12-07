5f767664901c2ecfa8533ac529b86669b69d2bf8
NandonBr1jveld <brandon.nijveld@student.nhlstenden.com>
2022-12-07 12:15:48 +0100

    Start of contact form

--- contact.php
+++ contact.php
@@ -5,13 +5,16 @@
     <img id="herobannerimg" src="./images/herobanner.png" alt="herobannerhome">
 </div>
 <main>
-    <h1>De morgenster laat je stralen!</h1>
-    <p>Welkom bij de christelijke basisschool De Morgenster. De school staat in de Zwolse wijk Westenholte. De Morgenster laat je stralen; door naar ieder individueel kind te kijken, kunnen we het beste aansluiten op de behoeften van die leerling en er is ruimte voor zijn of haar talenten. Dit doen we door onderzoekend en ontdekkend leren. We dagen de leerlingen uit om vanuit een natuurlijke nieuwsgierigheid de wereld te ontdekken. Dit doen we door op het onderwijs thematisch aan te bieden waarbij meerdere vakken geïntegreergd worden. We leren onze leerlingen hierdoor samen te werken, verantwoordelijkheid te nemen, probleemoplossend te denken en creatief te zijn. </p>
-    <p>We kijken op welke manier een leerling, in deze groep, bij deze leerkracht, met deze ouders, zich het beste kan ontwikkelen. Iedere leerling is hierbij eigenaar van zijn of haar eigen onderwijsproces.</p>
-    <h1>‘Op De morgenster is voor ieder kind een plek en kan ieder kind stralen.’</h1>
-    <p>De waarden van de Morgenster zijn: samen, open en waardevol. Leerlingen, ouders en team werken samen aan waardevol onderwijs door zelf open te zijn en een open houding te hebben naar de ander. Dit zijn waarden die niet alleen bij de school passen, maar ook bij de wijk. Westenholte is een wijk waarin de saamhorigheid van groot belang is.</p>
-    <h1>‘Wij zijn in staat een veilig klimaat te scheppen waar kinderen opbloeien, zodat ze echt kunnen leren.’</h1>
-    <p>Op onze school zitten ongeveer 150 leerlingen, hierdoor staat elk kind centraal. Waardoor het leerproces extra persoonlijk wordt gerealiseerd. Het plezier van de leerlingen is een stralend gezicht voor onze school. Wij als school denken ook aan de duurzaamheid, in de school wordt er geen onnodig papier gebruikt en zijn al onze potloden zijn gemaakt van duurzaam hout. Wij vinden het belangrijk om de leerlingen een beeld te geven hoe belangrijk duurzaamheid is. Door dit jong mee te geven, wordt het later vanzelfsprekend.</p>
+    <h1>Contact</h1>
+    <form action="#" method="post">
+        <label>Naam:</label>
+        <input type="text" id="naam"  name="naam" placeholder="Vul uw naam in..." required><br>
+        <label>Email:</label>
+        <input type="email" id="email"  name="email" placeholder="Vul uw mailadres in..." required><br>
+        <label>Telefoonnummer:</label>
+        <input type="tel" id="telefoon"  name="telefoon" placeholder="Vul uw telefoonnummer in..."><br>
+        <label>Bericht:</label>
+    </form>
 </main>
 
 <?php include('footer.php'); ?>
