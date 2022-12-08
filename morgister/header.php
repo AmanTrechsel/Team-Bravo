3ff6f6d16c6f21fe7ca0961dfb0ffc54fefff7e9
KevinSpa2 <kevin.spalink@student.nhlstenden.com>
2022-12-08 12:26:58 +0100

    Start student tracking system - all files

---
+++ morgister/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Cambo' rel='stylesheet'>
+    <title>De morgenster</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+<div id="container">
+    <header>
+        <div id="logo">
+            <img src="img/logo.png">
+        </div>
+        <ul>
+            <li>Profiel</li>
+            <li>Uitloggen</li>
+        </ul>
+
+    </header>
