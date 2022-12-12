02f22c546c7b38b1f7daede9dc5eb2365064819a
Timdegeus <tim.de.geus@student.nhlstenden.com>
2022-12-12 16:32:25 +0100

    Added grades.php, Added a grade list. Added pupilGradeList.php, added a pupil list. Added css for grades.php and pupilGradeList.php

---
+++ morgister/pupilGradeList.php
@@ -0,0 +1,31 @@
+<?php require('header.php'); ?>
+
+<div id="mainGrades">
+    <div id="gradesContent">
+        <table>
+            <tr>
+                <th class="subjectTable"><h2>Leerlingen</h2></th>
+            </tr>
+            <tr>
+                <td class="semesterTable"><a href="grades.php">John Doe</a></td>
+            </tr>
+            <tr>
+                <td class="semesterTable"><a href="grades.php">John Doe</a></td>
+            </tr>
+            <tr>
+                <td class="semesterTable"><a href="grades.php">John Doe</a></td>
+            </tr>
+            <tr>
+                <td class="semesterTable"><a href="grades.php">John Doe</a></td>
+            </tr>
+            <tr>
+                <td class="semesterTable"><a href="grades.php">John Doe</a></td>
+            </tr>
+            <tr>
+                <td class="semesterTable"><a href="grades.php">John Doe</a></td>
+            </tr>
+        </table> 
+    </div>
+</div>
+
+<?php require('footer.php'); ?>
