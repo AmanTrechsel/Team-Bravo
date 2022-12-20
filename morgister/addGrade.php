86026ce9417eb65046cf65aa2dbc3bb859e8fe6f
Timdegeus <tim.de.geus@student.nhlstenden.com>
2022-12-20 10:10:12 +0100

    Added multiple pages for Grades. Added PHP to all pages. Added CSS to all pages

---
+++ morgister/addGrade.php
@@ -0,0 +1,55 @@
+<?php require('header.php'); 
+ 
+ if(isset($_GET['scholar_id'])&& ($_GET['subject_id'])){
+    $scholar = $_GET['scholar_id'];
+    $subject = $_GET['subject_id'];
+?>
+
+<div id="mainGrades">
+    <div id="gradesTitle">
+        <?php echo "<a href='gradeList.php?scholar_id=" . $scholar . "&subject_id=" . $subject . "'><p id='leerlingButton'>Vorige pagina</p></a>" ?>
+    </div>
+    <div id="gradesContent">
+        <h2 class="addgradeTitle">Cijfer toevoegen</h2>
+    </div>
+    <div class="gradeformContent">
+       <?php echo "<form action='addGrade.php?scholar_id=" . $scholar . "&subject_id=" . $subject . "' method='POST' enctype='multipart/form-data' class='gradeForm'>" ?>
+            <div>          
+                <label for="Product">Cijfer: </label>
+                <input type="number" min="1" max="10" name="grade" id="grade">
+                <input type="submit" value="Toevoegen">
+            </div>
+        </form>
+
+        <?php
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            $err=[];
+            $grade = filter_input(INPUT_POST, 'grade', FILTER_SANITIZE_SPECIAL_CHARS);
+            
+            try {
+                $g_dbHandler = new PDO("mysql:host=mysql;dbname=Morgister", "root", "qwerty");
+
+
+            $stmt = $g_dbHandler->prepare("INSERT INTO `Grades` (`grade_id`, `grade`, `subject_id`, `scholar_id`) 
+            VALUES (NULL, :grade, :subject_id, :scholar_id)");
+
+            $stmt->bindParam('grade', $grade, PDO::PARAM_INT);
+            $stmt->bindParam('subject_id', $subject, PDO::PARAM_INT);
+            $stmt->bindParam('scholar_id', $scholar, PDO::PARAM_INT);
+            $stmt->execute();
+            } catch(Exception $ex) {
+                echo $ex;
+        }
+        ?>
+        <p>Cijfer succesvol toegevoegd</p>
+    </div>
+  
+    <?php
+        }
+    }
+    ?>
+</div>
+
+
+<?php require('footer.php'); ?>
---
+++ morgister/subjectList.php
@@ -0,0 +1,58 @@
+<?php require('header.php'); 
+ 
+ if(isset($_GET['scholar_id'])){
+    $scholar = $_GET['scholar_id'];
+    
+?>
+
+<div id="mainGrades">
+    <div id="gradesTitle">
+        <a href="scholarList.php"><p id="leerlingButton">Leerlingen</p></a>
+    </div>
+    <div id="gradesContent">
+        <table>
+            <tr>
+                <td class="subjectTable"><h2>Vakken</h2></td>
+                <td class="subjectTable"><h2></h2></td>
+            </tr>
+
+            <?php 
+
+                try {
+                    $g_dbHandler = new PDO("mysql:host=mysql;dbname=Morgister", "root", "qwerty");
+                } catch(Exception $ex) {
+                    echo $ex;
+                }
+
+                $stmt = $g_dbHandler->prepare("SELECT * FROM Subjects");
+
+                $stmt->execute();
+        
+                if($stmt->rowCount() > 0):
+                        $rows = $stmt->fetchAll();
+                        foreach($rows as $row):
+
+                ?>
+
+                <tr>
+                    <td class="semesterTable"><?php echo $row['subject_name']; ?></td>
+                    <td class="semesterTable"><?php echo "<a href='gradeList.php?scholar_id=" . $scholar . "&subject_id=" . $row['subject_id'] . "'>Bekijk cijfers</a>" ?></td> 
+                </tr>
+
+                <?php
+
+                endforeach;
+
+                endif;
+
+                $g_dbHandler = null;
+            }
+            
+            ?>
+
+        </table> 
+    </div>
+</div>
+
+
+<?php require('footer.php'); ?>
